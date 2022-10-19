ecedc33133608d3f5ebdd8048ab7ef9cfbf44943
Tanya <voznyuk1998@mail.ru>
2022-10-19 19:24:13 +0300

    Init commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,12 @@
-const { merge } = require('webpack-merge');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin  = require ("css-minimizer-webpack-plugin") ;
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   optimization: {
     minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      '...',
-    ],
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})], //,
   },
 });
